70ffb2d2e6bf5ad1fae9937cc5225ecf7e2ceffd
LMeister <136544719+MisterMeister@users.noreply.github.com>
2023-06-21 20:22:11 +0800

    .

--- Mokocchi Bot/commands/information/invite.js
+++ Mokocchi Bot/commands/information/invite.js
@@ -1,14 +1,16 @@
 const turl = require('turl');
 const { SlashCommandBuilder } = require('discord.js');
-const { inviteLink } = require('./config.json');
+// const { inviteLink } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('invite')
 		.setDescription('Sends the bot ionvite link for Mokocchi'),
 	async execute(interaction) {
+        const inviteLink = "https://discord.com/api/oauth2/authorize?client_id=1118382149368807496&permissions=8&scope=applications.commands%20bot"
         turl.shorten(inviteLink).then((res) => {
             interaction.reply(res);
+            console.log(res);
         }).catch((err) => {
             console.log(err);
         });
