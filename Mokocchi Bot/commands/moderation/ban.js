ab8d232bb2f06f7439cf55fde264615026d392ba
LMeister <136544719+MisterMeister@users.noreply.github.com>
2023-07-05 21:37:01 +0800

    Added optional ban reason
    Check situation where bot has admin but the ban target doesn't

--- Mokocchi Bot/commands/moderation/ban.js
+++ Mokocchi Bot/commands/moderation/ban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, BanOptions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,7 +7,11 @@ module.exports = {
         .addUserOption(option =>
             option.setName("user")
             .setDescription("the user to be banned")
-            .setRequired(true)),
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName("reason")
+            .setDescription("reason for the ban")
+            .setRequired(false)),
 	async execute(interaction) {
 
         try{
@@ -19,8 +23,11 @@ module.exports = {
 
             console.log(interaction.member.permissions)
 
-            console.log(`is the user bannable: ${userToBeBanned.bannable}`)
+            console.log(`is the user bannable: ${userToBeBanned.moderatable}`)
             var banPerms = true;
+            var banReason = interaction.options.getString("reason");
+            console.log(banReason)
+
 
             try {
                 banPerms = interaction.member.permissions.any("BAN_MEMBERS");
@@ -28,14 +35,20 @@ module.exports = {
                 banPerms = false; 
             }
             console.log(`can you ban: ${banPerms}`)
+
+            if(banReason === null) {
+                banReason = "No reason provided"
+            }
             if((userToBeBanned.bannable) && (banPerms)) {
-                guildOfBan.members.ban(userToBeBanned)
+                guildOfBan.members.ban(userToBeBanned, {
+                    reason: `${banReason}`
+                })
                 await interaction.reply(`${bannedMemberUsername} has been banned`)
             } else {
                 if(!userToBeBanned.bannable) {
                     await interaction.reply(`#ERROR: ${bannedMemberUsername} cannot be banned by Mokocchi Bot (Bot has lower hierarchy)`)
                 } else {
-                    await interaction.reply("#ERROR: Invalid permissions (this command can only be used by guild members with ban permissions)")
+                    await interaction.reply("#ERROR: You cannot ban a member above you/You lack ban permissions")
                 }
             }
             
