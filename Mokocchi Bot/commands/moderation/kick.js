48b899a21a49d287e1163fcdc363bcf6f4f16ebf
LMeister <136544719+MisterMeister@users.noreply.github.com>
2023-07-11 23:15:28 +0800

    filled kick.js, replace ban with kick

---
+++ Mokocchi Bot/commands/moderation/kick.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder, BanOptions } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kick an user')
+        .addUserOption(option =>
+            option.setName("user")
+            .setDescription("the user to be kicked")
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName("reason")
+            .setDescription("reason for the ban")
+            .setRequired(false)),
+	async execute(interaction) {
+
+        try{
+
+            const guildOfBan = interaction.guild;
+
+            const userToBeBanned = interaction.options.getMember("user"); 
+            const bannedMemberUsername = userToBeBanned.user.username;
+
+            console.log(interaction.member.permissions)
+
+            console.log(`is the user bannable: ${userToBeBanned.moderatable}`)
+            var banPerms = true;
+            var banReason = interaction.options.getString("reason");
+            console.log(banReason)
+
+
+            try {
+                banPerms = interaction.member.permissions.any("BAN_MEMBERS");
+            } catch(err) {
+                banPerms = false; 
+            }
+            console.log(`can you ban: ${banPerms}`)
+
+            if(banReason === null) {
+                banReason = "No reason provided"
+            }
+            if((userToBeBanned.bannable) && (banPerms)) {
+                guildOfBan.members.ban(userToBeBanned, {
+                    reason: `${banReason}`
+                })
+                await interaction.reply(`${bannedMemberUsername} has been banned`)
+            } else {
+                if(!userToBeBanned.bannable) {
+                    await interaction.reply(`#ERROR: ${bannedMemberUsername} cannot be banned by Mokocchi Bot (Bot has lower hierarchy)`)
+                } else {
+                    await interaction.reply("#ERROR: You cannot ban a member above you/You lack ban permissions")
+                }
+            }
+            
+        } catch(err) {
+            console.log(err)
+        }
+
+        
+	},
+};
