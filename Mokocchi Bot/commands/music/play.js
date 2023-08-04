3f185c93fb513b0d456b92992aadb20099314c35
LMeister <136544719+MisterMeister@users.noreply.github.com>
2023-08-04 13:59:15 +0800

    updated play.js

---
+++ Mokocchi Bot/commands/music/play.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, BanOptions } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+		.setDescription('play a song in a vc you are in')
+        .addStringOption(option =>
+            option.setName("song")
+            .setDescription("the yt link to be played")
+            .setRequired(true)),
+
+	async execute(interaction) {
+
+        var channel = interaction.member.voice.channel
+
+        if(!channel) {
+            interaction.reply(`"#ERROR: You must be in a voice channel in order to play music" ${channel}`)
+        } else {
+            interaction.reply(`Now playing '${interaction.options.getString("song")}, status: ${channel}'`)
+        }
+
+
+        // if(!interaction.member)
+
+    }
+}
